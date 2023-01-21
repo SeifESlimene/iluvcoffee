f5853749aedfdd07a039bb16048e9befbef5b22c
seifeslimene <s.slimene19@gmail.com>
2023-01-21 06:09:17 +0100

    add full crud coffee service with entity with controller that calls them

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,32 +1,33 @@
 import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res, Patch, Delete, Query } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+	constructor(private readonly coffeesService: CoffeesService) {}
+
 	@Get()
 	findAll(@Query() paginationQuery) {
-		const { limit, offset } = paginationQuery
-		return `This action return all coffees. Limit: ${limit}, offset: ${offset}`
+		// const { limit, offset } = paginationQuery 
+		return this.coffeesService.findAll()
 	}
 	
 	@Get(':id')
 	findOne(@Param('id') id: string) {
-		return `This action return #${id} coffee`
+		return this.coffeesService.findOne(id)
 	}
 
 	@Post()
-	 @HttpCode(HttpStatus.GONE)
 	create(@Body() body) {
-		return body
-		// return `This action creates a coffee`
+		return this.coffeesService.create(body)
 	}
 
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() body) {
-		return `This action updates #${id} coffee`
+		return this.coffeesService.update(id, body)
 	}
 
 	@Delete(':id')
 	remove(@Param('id') id: string) {
-		return `This action removes #${id} coffee`
+		return this.coffeesService.remove(id)
 	}
 }
