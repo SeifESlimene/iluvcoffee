5c325a6436f74f3dff21e66df347579c8da8d4f2
seifeslimene <s.slimene19@gmail.com>
2023-01-23 23:46:57 +0100

    request scoped providers

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res, Patch, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res, Patch, Delete, Query, Inject } from '@nestjs/common';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Request } from 'express'
+import { REQUEST } from '@nestjs/core';
 
 @Controller('coffees')
 export class CoffeesController {
-	constructor(private readonly coffeesService: CoffeesService) {}
+	constructor(private readonly coffeesService: CoffeesService, @Inject(REQUEST) private readonly request: Request) {
+		console.log('CoffeesController created')
+		console.log(request.headers)
+	}
 
 	@Get()
 	findAll(@Query() paginationQuery: PaginationQueryDto) {
