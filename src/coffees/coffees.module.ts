4c6f86df63ace277e557be580c943fb73691a3ff
seifeslimene <s.slimene19@gmail.com>
2023-01-23 21:41:56 +0100

    nonclassbased providers tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,15 +5,16 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 class MockCoffeesService {}
 
 @Module({ 
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
-    providers: [{
-        provide: CoffeesService,
-        useValue: new MockCoffeesService()
+    providers: [CoffeesService, {
+        provide: COFFEE_BRANDS,
+        useValue: ['buddy brew', 'nescafe']
     }],
     exports: [CoffeesService]
 })
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,5 +1,5 @@
 import { PaginationQueryDto } from './../common/dto/pagination-query.dto';
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -7,6 +7,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Injectable()
 export class CoffeesService {
@@ -15,8 +16,11 @@ export class CoffeesService {
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
-        private readonly connection: Connection
-    ) {}
+        private readonly connection: Connection,
+        @Inject(COFFEE_BRANDS) coffeeBrands: string[]
+    ) {
+        console.log(coffeeBrands)
+    }
 
 	findAll(paginationQuery: PaginationQueryDto) {
         const { limit, offset } = paginationQuery 
