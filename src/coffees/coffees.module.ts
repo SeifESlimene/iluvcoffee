747fd7fb0afd9b7c23acb2f31fd8049d0ab0c35f
seifeslimene <s.slimene19@gmail.com>
2023-01-23 23:12:28 +0100

    async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,25 +6,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
-
-@Injectable()
-export class CoffeeBrandsFactory {
-    create() {
-        // Do Something
-        return ['buddy brew', 'nescafe']
-    }
-}
+import { Connection } from 'typeorm';
 
 @Module({ 
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
-        CoffeeBrandsFactory,
         { 
             provide: COFFEE_BRANDS, 
-            useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
-            inject: [CoffeeBrandsFactory]
+            useFactory: async (connection: Connection): Promise<string[]> => {
+                // const coffeeBrands = await connection.query('SELECT * ...')
+                const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe'])
+                console.log('[!] Async factory')
+                return coffeeBrands
+            },
+            inject: [Connection]
         }
     ],
     exports: [CoffeesService]
