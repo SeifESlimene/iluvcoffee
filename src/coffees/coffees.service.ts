5eed92b2cb92402e814b6c888308583b3d28095b
seifeslimene <s.slimene19@gmail.com>
2023-01-23 01:38:48 +0100

    use transactions

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,11 +1,12 @@
 import { PaginationQueryDto } from './../common/dto/pagination-query.dto';
 import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -13,7 +14,8 @@ export class CoffeesService {
         @InjectRepository(Coffee)
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
-        private readonly flavorRepository: Repository<Flavor>
+        private readonly flavorRepository: Repository<Flavor>,
+        private readonly connection: Connection
     ) {}
 
 	findAll(paginationQuery: PaginationQueryDto) {
@@ -71,5 +73,29 @@ export class CoffeesService {
             return existingFlavor
         }
         return this.flavorRepository.create({ name })
+    }
+
+    async recommendCoffee(coffee: Coffee) {
+        const queryRunner = this.connection.createQueryRunner()
+
+        await queryRunner.connect()
+        await queryRunner.startTransaction()
+        try {
+            coffee.recommendations++
+            
+            const recommendEvent = new Event()
+            recommendEvent.name = 'recommend_coffee'
+            recommendEvent.type = 'coffee'
+            recommendEvent.payload = { coffeeId: coffee.id }
+
+            await queryRunner.manager.save(coffee)
+            await queryRunner.manager.save(recommendEvent)
+
+            await queryRunner.commitTransaction()
+        } catch (err) {
+            await queryRunner.rollbackTransaction()
+        } finally {
+            await queryRunner.release()
+        }
     }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -11,6 +11,9 @@ export class Coffee {
 
     @Column()
     brand: string
+
+    @Column({ default: 0 })
+    recommendations: number
  
     @JoinTable()
     @ManyToMany(
