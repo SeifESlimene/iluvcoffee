06cec777897463473a4c4946fba1ce1b47a0665e
seifeslimene <s.slimene19@gmail.com>
2023-01-26 18:24:50 +0100

    custom configuration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config'
 import * as Joi from '@hapi/joi';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     CoffeesModule,
     ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432)
-      })
+      load: [appConfig]
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
