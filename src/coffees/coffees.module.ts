1d245323c2714ee888679f8d6c8976d14208a5d9
seifeslimene <s.slimene19@gmail.com>
2023-01-26 18:31:26 +0100

    partial registration

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,9 +6,10 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { ConfigModule } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
 
 @Module({ 
-    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule.forFeature(coffeesConfig)],
     controllers: [CoffeesController],
     providers: [CoffeesService],
     exports: [CoffeesService]
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -19,8 +19,8 @@ export class CoffeesService {
         private readonly connection: Connection,
         private readonly configService: ConfigService,
     ) {
-        const databaseHost = this.configService.get('database.host', 'localhost')
-        console.log(databaseHost)  
+        const coffeesConfig = this.configService.get('coffees.foo')
+        console.log(coffeesConfig)  
     }
 
 	findAll(paginationQuery: PaginationQueryDto) {
