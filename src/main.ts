d28898558d39082b976329e043c1c300d72a693c
seifeslimene <s.slimene19@gmail.com>
2023-01-27 01:47:40 +0100

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,13 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor())
+
+
+  const options = new DocumentBuilder().setTitle('Iluvcoffee').setDescription('Coffee application').setVersion('1.0').build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+
   await app.listen(3000);
 }
 bootstrap();
